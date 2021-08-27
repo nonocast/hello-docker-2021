d0be57d2fca5e767e6bc9e729ba397b65b4b0feb
Hui Cao <nonocast@gmail.com>
2021-08-27 16:24:33 +0800

    initial

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <h1>demo page</h1>
+      <p>{props.message}</p>
+    </div>
+  );
+}
+
+let hoc = WrappedComponent => {
+  return class EnhancedComponent extends React.Component {
+    constructor() {
+      super();
+      this.state = { message: 'empty' };
+    }
+
+    async componentDidMount() {
+      let resp = await axios.get('http://localhost:9000');
+      this.setState({ message: resp.data });
+    }
+
+    render() {
+      console.log('render', this.state.message);
+      return <WrappedComponent message={this.state.message} />;
+    }
+  };
+};
+
+export default hoc(App);
